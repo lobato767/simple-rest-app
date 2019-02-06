be64e50e679f052d531585239ca19f24493888fe
Matheus Mota <smota.matheus@gmail.com>
2019-02-05 23:20:36 -0200

    uploading example

---
+++ src/server.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'v1 - Hello from the simple-rest-app!!!!!'
+
+@app.route('/rota2')
+def rota2():
+    return 'v1 - Hello from the simple-rest-app rota2!'
+
+
+if __name__ == '__main__':
+	app.run(host="0.0.0.0", port=8181, debug=True)
